517773a7862917405800d9de2b3e933f8e572896
elinfinitoindia <elinfinitoindia@gmail.com>
2018-02-24 01:23:08 +0530

    contact_booking_cancelltaion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {TicketComponent} from './shared/ticket.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BookingComponent } from './booking/booking.component';
+import { CancellationComponent } from './cancellation/cancellation.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TicketComponent
+    TicketComponent,
+    AboutusComponent,
+    BookingComponent,
+    CancellationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
