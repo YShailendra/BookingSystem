ea476700448ccef7b29a8de9246249ae6d59abbe
@ershailendra57 <ershailendra57@gmail.com>
2018-02-24 01:12:48 +0530

    Routing updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {TicketComponent} from './shared/ticket.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing-module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TicketComponent
+    TicketComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
