9fbe20a1abb6a1c9b680781d33fcdc22a31ca069
elinfinitoindia <elinfinitoindia@gmail.com>
2018-02-24 08:01:17 +0530

    routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,6 +4,13 @@ import { LoginComponent } from './login/login.component';
 
 import { RouteGuard } from './guards/route.guard';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {BookingComponent} from './booking/booking.component';
+import {CancellationComponent} from './cancellation/cancellation.component';
+import {ContactComponent} from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 //roles 1-Analyst,2-Admin 3-HotelManager 4-CEO
 const routes: Routes = [
@@ -14,7 +21,23 @@ const routes: Routes = [
   },
   {
     path: '**',//default url
-    component: NotFoundComponent
+    component: HomeComponent
+  },
+  {
+    path:'about',
+    component: AboutusComponent
+  },
+  {
+    path:'booking',
+    component: BookingComponent
+  },
+  {
+    path:'cancellation',
+    component: CancellationComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
   },
   
 ];
