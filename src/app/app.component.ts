565618db4d9fdcb571334deb3b67551cb397cc77
elinfinitoindia <elinfinitoindia@gmail.com>
2018-03-27 13:04:09 +0530

    validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { SharedService} from '../app/services/shared.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
-  
-  constructor(private toastrService: ToastrService)
+  public isAdmin;
+  constructor(private toastrService: ToastrService,private sharedService:SharedService)
   {
     
   }
+
+  ngOnInit(){
+      this.sharedService.isLoggedIn.subscribe(data=>{
+        this.isAdmin = data;
+      })
+  }
+
 }
